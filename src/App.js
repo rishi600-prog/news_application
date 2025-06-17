1a51bc86c6a08e2e2ed07b7f79568cb67de0b4f4
rishi_s_mohan <srishimohan600@gmail.com>
2025-06-17 17:37:51 +0530

    feat: move news fetching logic to Home component and simplify App component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
-import NewsCard from './components/NewsCard';
-import Footer from './components/Footer';
-import { fetchNews } from './api/news';
+import React from 'react';
+import Home from './pages/Home';
 
 function App() {
-  const [articles, setArticles] = useState([]);
-  const [category, setCategory] = useState('general');
-  const [country, setCountry] = useState('in');
-  const [query, setQuery] = useState('');
-
-  useEffect(() => {
-    const getNews = async () => {
-      const data = await fetchNews(country, category, query);
-      setArticles(data);
-    };
-    getNews();
-  }, [category, country, query]);
-
-  return (
-    <div className="min-h-screen bg-gray-100 flex flex-col">
-      <Navbar
-        onCategoryChange={setCategory}
-        onCountryChange={setCountry}
-        onSearch={(value) => {
-          setQuery(value);
-          if (value.length > 0) {
-            setCategory('');
-            setCountry('');
-          }
-        }}
-      />
-      <main className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {articles.map((article, index) => (
-          <NewsCard key={index} article={article} />
-        ))}
-      </main>
-      <Footer />
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import Navbar from '../components/Navbar';
+import NewsCard from '../components/NewsCard';
+import Footer from '../components/Footer';
+import { fetchNews } from '../api/news';
+
+function Home() {
+  const [articles, setArticles] = useState([]);
+  const [category, setCategory] = useState('general');
+  const [country, setCountry] = useState('in');
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const getNews = async () => {
+      const data = await fetchNews(country, category, query);
+      setArticles(data);
+    };
+    getNews();
+  }, [category, country, query]);
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      <Navbar
+        onCategoryChange={setCategory}
+        onCountryChange={setCountry}
+        onSearch={(value) => {
+          setQuery(value);
+          if (value.length > 0) {
+            setCategory('');
+            setCountry('');
+          }
+        }}
+      />
+      <main className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {articles.map((article, index) => (
+          <NewsCard key={index} article={article} />
+        ))}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
